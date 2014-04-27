0385d5881d4a77c076c54491194963ff39d0c504
Andrew Yager <andrew@rwts.com.au>
2014-04-27 15:57:12 +1000

    Update setup.py to read markdown readme file for long description

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
 from setuptools import setup
 
+long_description = ''
+ 
+try:
+    import subprocess
+    import pandoc
+ 
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+ 
+    pandoc_path = process.communicate()[0]
+    pandoc_path = pandoc_path.strip('\n')
+ 
+    pandoc.core.PANDOC_PATH = pandoc_path
+ 
+    doc = pandoc.Document()
+    doc.markdown = open('README.md').read()
+ 
+    long_description = doc.rst
+ 
+except:
+    pass
+
+
 setup(
 	name="asterisk_dialplan",
-	version="0.1.1",
+	version="0.1.2",
 	author="Andrew Yager",
 	author_email="andrew@rwts.com.au",
 	license="BSD",
 	packages=['asterisk_dialplan'],
 	description="Helpers to convert numbers to dialplan strings for use in Asterisk",
+	long_description=long_description,
 	classifiers=[
 		"Development Status :: 4 - Beta",
 		"Topic :: Communications :: Telephony",
